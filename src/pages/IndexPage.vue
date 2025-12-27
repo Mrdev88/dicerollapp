e74f8a4e5cf3d701832743ba7d68419780680cb4
Mahdi <RuizMahdi@gmx.com>
2025-12-27 12:25:57 -0500

    initial commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,40 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page class="old-style flex flex-center column q-gutter-md">
+    <h1>Dicerollapp</h1>
+    <h2>Created by Mahdi Ruiz</h2>
+
+    <q-select
+      v-model="dice"
+      :options="diceOptions"
+      label="Select a dice"
+      emit-value
+      map-options
+      style="width: 200px"
     />
+
+    <q-btn @click="rollDice" :disable="!dice" label="Roll Dice" color="primary" />
+
+    <p v-if="result" class="text-h6">You rolled: {{ result }}</p>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+
+const dice = ref('')
+const result = ref(null)
+
+const diceOptions = [
+  { label: 'D4', value: '4' },
+  { label: 'D6', value: '6' },
+  { label: 'D8', value: '8' },
+  { label: 'D10', value: '10' },
+  { label: 'D12', value: '12' },
+  { label: 'D20', value: '20' },
+]
+
+const rollDice = () => {
+  if (!dice.value) return
+  result.value = Math.floor(Math.random() * parseInt(dice.value)) + 1
+}
 </script>
